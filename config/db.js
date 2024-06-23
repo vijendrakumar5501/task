6093f196eb3c29560bd6850df49e0f489bba75e5
vijendrakumar <vijendrakumar5501@gmail.com>
2024-06-23 21:34:34 +0530

    db up

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-// GStdvSIoYYpzo3tu
+
 const uri = "mongodb+srv://vijendra:GStdvSIoYYpzo3tu@cluster0.kjqpqat.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(uri).then(() => {
