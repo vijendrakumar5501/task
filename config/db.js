c0331457efe60f83b4c24327525619fc9afa8c52
vijendrakumar <vijendrakumar5501@gmail.com>
2024-06-23 21:28:54 +0530

    db

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose=require('mongoose')
 
+// GStdvSIoYYpzo3tu
+const uri = "mongodb+srv://vijendra:GStdvSIoYYpzo3tu@cluster0.kjqpqat.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-
-mongoose.connect("mongodb://localhost:27017/taskCollection").then(() => {
+mongoose.connect(uri).then(() => {
     console.log("connected to mongodb");
   });
 
